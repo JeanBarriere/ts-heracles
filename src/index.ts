bfd9138d7a2ebd769fc108c984e87c967a6aa414
Jean Barriere <jean.barriere@shell.com>
2022-10-31 15:59:35 +0100

    wip: TDD prep

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,37 @@
-export function add(left: number, right: number): number {
-    return left + right
-}
+const listOfCities: string[] = ['Amsterdam', 'Rotterdam', 'Paris', 'Warsaw', 'Amsterdam', 'Amsterdam', 'Chicken']
+
+const setOfCities = new Set(listOfCities);
+
+console.log(setOfCities)
+// Set(5) { 'Amsterdam', 'Rotterdam', 'Paris', 'Warsaw', 'Chicken' }
+
+setOfCities.add('Roma')
+
+console.log(setOfCities)
+// Set(6) { 'Amsterdam', 'Rotterdam', 'Paris', 'Warsaw', 'Chicken', 'Roma' }
+
+setOfCities.delete('Chicken')
+
+console.log(setOfCities)
+// Set(5) { 'Amsterdam', 'Rotterdam', 'Paris', 'Warsaw', 'Roma' }
+
+const hasTheHague = setOfCities.has('The Hague')
+
+console.log({ hasTheHague })
+// { hasTheHague: false }
+
+console.log({ size: setOfCities.size })
+// { size: 5 }
+
+setOfCities.clear()
+
+console.log({ size: setOfCities.size })
+// { size: 0 }
+
+type AllowedCities = 'Amsterdam' | 'Rotterdam' | 'Paris' | 'Warsaw' | 'Roma' | 'The Hague'
+const setOfStrictCities = new Set<AllowedCities>()
+
+// @ts-expect-error parameter should be of type AllowedCities
+setOfStrictCities.add('Chicken')
+
+const setOfMyCities = new MySet(listOfCities);
